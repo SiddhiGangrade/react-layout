a8215bcfd51900eccff937a1caee6b1c3ec4d461
unknown <siddhigangrade@gmail.com>
2022-12-17 19:31:23 +0530

    kit react

--- src/App.js
+++ src/App.js
@@ -3,22 +3,201 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+
+      <section className='box-header'>
+        <div className='container'>
+          <div className='header'>
+
+            <nav className="navbar navbar-expand-lg ">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img src='https://capp.nicepage.com/229c15c744a3e30e774dfd4df23b93b58dc49eca/images/default-logo.png'></img> </a>
+                <a class="nav-link-active" aria-current="page" >Home</a>
+
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div className='header-one'>
+          <div className='header-center'>
+            <div className='container-layout'>
+              <h6 className='prime'>GREEN IS THE PRIME COLOR OF THE WORLD</h6>
+              <h1 className='nature'>Nature is pleased with simplicity</h1>
+              <h6 className='image'>IMAGE FROM <br />FREEPIK</h6>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='box-align-center'>
+        <div className='header-two'>
+          <div className='header-center-two'>
+            <div className='container'>
+
+              <div className='grid'>
+
+                <div class="row">
+
+                  <div class="col">
+                    <h1 className='text'>01</h1>
+                    <h2 className='awesome'>Awesome <b>Image</b></h2>
+                    <p className='para'>Paragraph. Lorem ipsum dolor sit amet, consectetur<br />
+                      adipiscing elit. Curabitur id suscipit ex. Suspendisse<br />
+                      rhoncus laoreet purus quis elementum. Phasellus sed<br />
+                      efficitur dolor, et ultricies sapien. Quisque fringilla<br />
+                      sit amet dolor commodo efficitur. Aliquam et sem odio.<br />
+                      In ullamcorper nisi nunc, et molestie<br />
+                      ipsum iaculis sit amet.</p>
+                  </div>
+
+                  <div class="col">
+                    <h1 className='text'>02</h1>
+                    <h2 className='awesome'>Fresh and <b>Creative</b></h2>
+                    <p className='para'>Paragraph. Lorem ipsum dolor sit amet, consectetur<br />
+                      adipiscing elit. Curabitur id suscipit ex. Suspendisse<br />
+                      rhoncus laoreet purus quis elementum. Phasellus sed<br />
+                      efficitur dolor, et ultricies sapien. Quisque fringilla<br />
+                      sit amet dolor commodo efficitur. Aliquam et sem odio.<br />
+                      In ullamcorper nisi nunc, et molestie<br />
+                      ipsum iaculis sit amet.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='sample-align-center'>
+        <div className='header-three'>
+
+          <div className='sample-text-layout'>
+            <div className='layout-row'>
+              <div className='layout-emage'>
+                <div className='container-layout-emage'>
+                  <p className='sample-text-box'>SAMPLE TEXT. CLICK TO SELECT THE TEXT BOX.<br /> CLICK AGAIN OR DOUBLE
+                    CLICK TO START EDITING <br /> THE TEXT.</p>
+                </div>
+              </div>
+
+              <div className='header-three-one'>
+                <div className='container-layout-image'>
+                  <h3 className='custom-font'>Green and <b>Healthy</b></h3>
+                  <button type="button" class="btn btn-outline-secondary">Read More</button>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+
+
+
+      <div className='container'>
+        <section className='card'>
+          <div className='card-sheet'>
+
+            <div className='img-card-sheet'>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/6300291be8d356b88ec77a6e/pexels-photo-3006003.jpeg'></img></div>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/4697ca9c085651e1b833b7c6/pexels-photo-3541389.jpeg'></img></div>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/2f1a3125d5735c5fae777d26/pexels-photo-2823459.jpeg'></img></div>
+            </div>
+
+
+            <div className='img-card-sheet'>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/c3da9eef93f251c98be4a77c/pexels-photo-1719641.jpeg'></img></div>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/334c2d1ad56e55a6add2b2d6/pexels-photo-3916422.jpeg'></img></div>
+              <div class="img-card"><img src='https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/323753a16fe7582eb1443667/pexels-photo-4099388.jpeg'></img></div>
+            </div>
+
+          </div>
+        </section>
+      </div>
+
+
+
+
+      <section className='floral design'>
+        <div className='floral'>
+          <div className='container'>
+            <div className='design'>
+              <div className='floral-layout'>
+                <div className='floral-layout-row'>
+                  <div className='floral-container'>
+                    <div className='floral-container-layout'>
+                      <h2 className='studio'>Floral <b>Design Studio</b></h2>
+                      <p className='custom para'>Custom interior studio, is a qualified interior designer,
+                        who comes from a Film &amp; TV background, during all
+                        the years she worked in the film industry, she had the
+                        opportunity to work hand in hand with the best art directors.
+                        This experience gave her great knowledge of design
+                        culture, the key to create ambiences for people to
+                        live in, and the most important thing,
+                        her passion for design.</p>
+                    </div>
+                  </div>
+
+                  <div className='personal-container'>
+                    <div className='personal-container-layout'>
+                      <p className='personal'>Personal attention, creative solutions. The studio is
+                        built on strong client relationship, personal
+                        attentiveness, ability to find the right solutions,
+                        and an eye for the details that make each space
+                        unique.Marks of identity:
+                        "To offer my clients a style that
+                        makes them feel comfortable and suits
+                        their personality".What defines my job: "The space,
+                        the object, the piece of furniture, the fabric, and any
+                        other element that helps me create
+                        the unique ambience my clients are looking for"</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+
+
+      <section className='green'>
+        <div className='container-green'>
+          <div className='container-layout-green'></div>
+        </div>
+        <div className='aterier-container'>
+          <div className='aterier-container-layout'>
+            <h2 className='this'>Green <b>Aterier</b></h2>
+            <h6 className='assistant'>THIS IS YOUR ASSISTANT</h6>
+            <h6 className='design-green'>IN DESIGN</h6>
+          </div>
+        </div>
+      </section>
+
+      <section className='subscribe'>
+        <h1 className='sub'>Subscribe</h1>
+        <div className='form'>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter a valid email address" />
+          <button type="button" class="primary">SUBMIT</button>
+        </div>
+      </section>
+
+      <section className='footer'>
+        <p className='sample-text'>Sample text. Click to select the text box. Click again or double <br />click to start editing the text.</p>
+
+        <p className='website'> <a href='#'>Website Templates</a> created with <a href='#'>Website Builder Software.</a></p >
+
+      </section>
+
+
+
+    </>
   );
 }
 
